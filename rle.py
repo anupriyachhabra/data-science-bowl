fc80de0b0c7e839e78916ad162f5a7924bd89f23
Anupriya Chhabra <anugoel1988@gmail.com>
2018-02-03 12:41:24 +1100

    better results with skmorphology

--- rle.py
+++ rle.py
@@ -3,31 +3,38 @@ from glob import glob
 import os
 from skimage import util,io
 import skimage
+from skimage.segmentation import clear_border
 import csv
 from skimage.morphology import label # label regions
+from skimage.color import label2rgb
 
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
 def rle_encoding(x):
-    '''
-    x: numpy array of shape (height, width), 1 - mask, 0 - background
-    Returns run length as list
-    '''
-    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    dots = np.where(x.T.flatten() == 1)[0]
     run_lengths = []
     prev = -2
     for b in dots:
-        if (b>prev+1): run_lengths.extend((b+1, 0))
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
         run_lengths[-1] += 1
         prev = b
-    return " ".join(map(str,run_lengths))
+    return run_lengths
 
-def prob_to_rles(x, cut_off = 0.5):
-    lab_img = label(x>cut_off)
-    if lab_img.max()<1:
-        lab_img[0,0] = 1 # ensure at least one prediction per image
-    for i in range(1, lab_img.max()+1):
-        yield rle_encoding(lab_img==i)
+def prob_to_rles(x, image_path):
+    # remove artifacts connected to image border
+    #cleared = clear_border(x)
+    # label image regions
+    lab_img = label(x)
+    skimage.io.imsave("label_runs/"+image_path+".png",  label2rgb(lab_img, x))
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
+
+
+def rle_to_string(runs):
+    return ' '.join(str(x) for x in runs)
 
 def prepare_submission():
+    new_test_ids = []
+    rles = []
     image_paths = glob('runs/1517306309.1110826/*')
     f = open('stage1_final_submission.csv', 'wt')
     writer = csv.writer(f)
@@ -36,13 +43,16 @@ def prepare_submission():
         image = skimage.io.imread(image_path, as_grey=True)
         #skimage.io.imsave("invert_"+image_path, util.invert(image))
         #gaussian blur after improving algo
-        print(os.path.basename(os.path.splitext(image_path)[0]))
+        #print(os.path.basename(os.path.splitext(image_path)[0]))
         #print(rle_encoding(util.invert(image)))
+        id_ = os.path.basename(os.path.splitext(image_path)[0])
+        rle = list(prob_to_rles(image, id_))
+        rles.extend(rle)
+        new_test_ids.extend([id_] * len(rle))
 
-        rles = prob_to_rles(util.invert(image), cut_off = 0.5)
-        rles_string = list(rles)
-        rles_string =  " ".join(map(str,rles_string))
-        writer.writerow( (os.path.basename(os.path.splitext(image_path)[0]), rles_string))
+    for i in range(0, len(new_test_ids)):
+        writer.writerow((new_test_ids[i], rle_to_string(rles[i])))
+
         #write to a file
 
 if __name__ == '__main__':
